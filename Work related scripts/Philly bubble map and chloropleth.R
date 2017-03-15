38444c8c1aacd0725cea6f80cfeb5268005dfeb3
Geoffrey Kip <g_kip@live.com>
2017-03-14 21:15:26 -0400

    Update and rename Philly bubble map.R to Philly bubble map and chloropleth.R

--- Work related scripts/Philly bubble map and chloropleth.R
+++ Work related scripts/Philly bubble map and chloropleth.R
@@ -6,6 +6,9 @@ library(dplyr)
 library(zipcode)
 library(plyr)
 library(viridis)
+library(maptools)
+library(rgdal)
+
 
 setwd("~/Documents/R files/BDT de-identified work/Benephilly_map/data")
 
@@ -22,7 +25,7 @@ benephilly_data$zip <-clean.zipcodes(benephilly_data$zip)
 benephilly_data2<- merge(benephilly_data, zipcode, by='zip')
 head(benephilly_data2)
 
-#get philly base map
+#get philly base map and make a bubble map
 philly <- get_map(location = "philadelphia", zoom=11, crop=T, scale="auto" ,
                   color="color" , maptype="terrain")
 
@@ -36,5 +39,37 @@ data_map <- philly_map + geom_point(aes(x = longitude, y = latitude, size = clie
                                     data = benephilly_data2, alpha = .5, pch=21, fill="red") +
                         ggtitle("Client Locations")
 data_map2<- data_map + scale_size(breaks = (c(0, 50, 100, 150, 200, 250)), labels = c(0, 50, 100, 150, 200, 250), name = "Clients Served")
-print(Legend)
+print(data_map2)
+
+
+#prepare chloropleth map
+#read shape file
+philly_shp <- readOGR("/Users/geoffrey.kip/Documents/R files/BDT de-identified work/Benephilly_map/PhillyStreets_Zipcodes_Arc/PhillyStreets_Zipcodes_Arc.shp")
+philly_shp@data$id <- rownames(philly_shp@data)
+philly_shp.point <- fortify(philly_shp, region="id")
+philly_shp.df <- inner_join(philly_shp.point,philly_shp@data, by="id")
+names(philly_shp.df)
+head(philly_shp.df)
+philly_shp.df$zip <- as.numeric(philly_shp.df$ZIP_LEFT)
+head(philly_shp.df)
+ggplot(philly_shp.df, aes(long, lat, group=group )) + geom_polygon()
+
+#prepare data
+benephilly_data2 <- rename(benephilly_data2,c('clients_served'='value'))
+philly_shp.df2 <- merge(philly_shp.df, benephilly_data2, by= "zip")
+
+
+# ggplot mapping
+# data layer
+m0 <- ggplot(data=philly_shp.df2)
+# empty map (only borders)
+m1 <- m0 + geom_path(aes(x=long, y=lat, group=group), color='gray') + coord_equal()
+m2 <- m1 + geom_polygon(aes(x=long, y=lat, group=group, fill=value))
+
+# inverse order (to have visible borders)
+m0 <- ggplot(data=philly_shp.df2)
+m1 <- m0 + geom_polygon(aes(x=long, y=lat, group=group, fill=value)) + coord_equal()
+m2 <- m1 + geom_path(aes(x=long, y=lat, group=group), color='black')
+m2
+
 
